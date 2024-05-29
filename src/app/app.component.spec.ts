69c439c2a64456875bd0e6e60d8f5160eda6d42c
ZhengLong <139528731+zhenglong1603@users.noreply.github.com>
2024-05-30 00:30:14 +0800

    more changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'victor lai'`, () => {
+  it(`should have as title 'zheng long'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('victor lai');
+    expect(app.title).toEqual('zheng long');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('victor lai app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('zheng long app is running!');
   });
 });
